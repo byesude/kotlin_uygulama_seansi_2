ad675953925fe1d41163ddc652b25ed4ff850190
Ecem Sude Ozan <137602264+byesude@users.noreply.github.com>
2023-07-26 21:09:04 +0300

    Add files via upload

---
+++ soru1/Main.kt
@@ -0,0 +1,8 @@
+package com.example.uygulamaseansi.uygulamaseansi2_26072023.soru1
+
+fun main() {
+    val dikdortgen = Dikdortgen(5.0, 3.0)
+
+    println("Dikdörtgenin Alanı: ${dikdortgen.alanHesapla()}")
+    println("Dikdörtgenin Çevresi: ${dikdortgen.cevreHesapla()}")
+}
