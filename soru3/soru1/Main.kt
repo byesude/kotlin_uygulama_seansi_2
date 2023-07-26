7a3aa48c26f641e856fbd58ada166d850224fe1c
Ecem Sude Ozan <137602264+byesude@users.noreply.github.com>
2023-07-27 00:33:02 +0300

    Delete soru3 directory

--- soru3/soru1/Main.kt
+++
@@ -1,8 +0,0 @@
-package com.example.uygulamaseansi.uygulamaseansi2_26072023.soru1
-
-fun main() {
-    val dikdortgen = Dikdortgen(5.0, 3.0)
-
-    println("Dikdörtgenin Alanı: ${dikdortgen.alanHesapla()}")
-    println("Dikdörtgenin Çevresi: ${dikdortgen.cevreHesapla()}")
-}
