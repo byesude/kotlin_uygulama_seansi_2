78acbe4ea3067dd9060167e9199045aba7d850cc
Ecem Sude Ozan <137602264+byesude@users.noreply.github.com>
2023-07-27 00:32:49 +0300

    Delete soru2 directory

--- soru2/Arac.kt
+++
@@ -1,4 +0,0 @@
-package com.example.uygulamaseansi.uygulamaseansi2_26072023.soru2
-
-open class Arac(val hiz : Int, val yakitTuketimi : Double){
-}
--- soru2/Otomobil.kt
+++
@@ -1,8 +0,0 @@
-package com.example.uygulamaseansi.uygulamaseansi2_26072023.soru2
-
-class Otomobil(hiz : Int, yakitTuketimi : Double, val bagajHacmi : Double) : Arac(hiz, yakitTuketimi)
-{
-    fun bagajAc() {
-        println("Bagaj açıldı.")
-    }
-}
